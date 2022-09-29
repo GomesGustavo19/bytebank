5f875438f32c1bc70e5c8d3b50c8438d6f7c487f
Gustavo Gomes <gugags2@hotmail.com>
2022-09-29 12:04:36 -0300

    Realizado a correção, e feito a inclusão de funcionarios, gerente

---
+++ src/main/java/model/Gerente.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Gerente extends Funcionario {
+
+    private int senha;
+
+    public double getBonificacao() {
+        return this.getSalario();
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public boolean autentica(int senha) {
+        if (this.senha == senha) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+}
